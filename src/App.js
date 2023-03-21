1c0b00b37fb53eb733ac4a2dbb1d104002e662ab
Chiara Arrivas <ritarrivas96@gmail.com>
2023-03-21 10:06:09 +0100

    todoLlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    setTodos((todos) => [...todos, { id: new Date().getTime(), content: input, isCompleted: false }])
+    setInput("")
+  }
+
+  const handleRemove = (index) => {
+    setTodos(
+      (todos) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1)
+        return newTodos
+      }
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleAdd}>
+        <input required type="text" value={input} onChange={(event) => setInput(event.target.value)} />
+        <button type="submit">ADD</button>
+      </form>
+      <div>
+        {todos.map((item, index) =>
+          <div key={item.id}>
+            <input type="checkbox" value={item.isCompleted} />{item.content}
+            <button onClick={() => handleRemove(index)}>X</button>
+          </div>)}
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
