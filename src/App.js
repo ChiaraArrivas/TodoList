67df6f7496ace5e3b17b4ceca8cb7c764d6b0547
Nico Barbieri <nicobarbieri.m@gmail.com>
2023-03-21 13:00:06 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import './index.css';
 
 function App() {
   const [input, setInput] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')))
 
   const handleAdd = (event) => {
     event.preventDefault();
@@ -10,32 +11,38 @@ function App() {
     setInput("")
   }
 
-  const handleRemove = (index) => {
-    setTodos(
-      (todos) => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1)
-        return newTodos
-      }
-    )
+  const handleRemove = (id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id))
   }
+  
+  useEffect(()=> {
+    localStorage.setItem('todos', JSON.stringify([...todos]))
+  }, [todos])
+
 
   return (
-    <div>
+    <div className="todo-app">
+      <h1>Windows 1.0 BIOS</h1>
       <form onSubmit={handleAdd}>
-        <input required type="text" value={input} onChange={(event) => setInput(event.target.value)} />
+        <input required type="text" value={input} onChange={(event) => setInput(event.target.value)} placeholder='type todo...'/>
         <button type="submit">ADD</button>
       </form>
-      <div>
-        {todos.map((item, index) =>
-          <div key={item.id}>
-            <input type="checkbox" value={item.isCompleted} />{item.content}
-            <button onClick={() => handleRemove(index)}>X</button>
-          </div>)}
+      <div className="todo-list-container">
+        <div className="todo-list-wrapper">
+          <div className="todo-list">
+            {todos.map((item) =>
+              <div key={item.id} className='todo'>
+                <div className="todo-content">
+                  <input type="checkbox" value={item.isCompleted} />
+                  <h3>{item.content}</h3>
+                  <button onClick={() => handleRemove(item.id)}>X</button>
+                </div>
+              </div>)}
+          </div>
+        </div>
       </div>
     </div>
   )
-
 }
 
 export default App;
